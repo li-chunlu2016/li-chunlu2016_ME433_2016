0b12fc753bc8baf06c9fc11d6e1a582311ecbd33
li-chunlu2016 <li-chunlu2016@u.northwestern.edu>
2016-04-26 11:07:41 -0500

    edit

--- HW6/IMU.X/main.c
+++ HW6/IMU.X/main.c
@@ -1,54 +1,62 @@
-#include<xc.h>           // processor SFR definitions
-#include<sys/attribs.h>  // __ISR macro
+#include<xc.h>                      // processor SFR definitions
+#include<sys/attribs.h>             // __ISR macro
 #include<math.h>
-#include "i2c_master_noint.h"
+#include "i2c_master_noint.h"       // I2C functions
 
 // DEVCFG0
-#pragma config DEBUG = OFF // no debugging
-#pragma config JTAGEN = OFF // no jtag
-#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
-#pragma config PWP = OFF // no write protect
-#pragma config BWP = OFF // no boot write protect
-#pragma config CP = OFF // no code protect
+#pragma config DEBUG = OFF          // no debugging
+#pragma config JTAGEN = OFF         // no jtag
+#pragma config ICESEL = ICS_PGx1    // use PGED1 and PGEC1
+#pragma config PWP = OFF            // no write protect
+#pragma config BWP = OFF            // no boot write protect
+#pragma config CP = OFF             // no code protect
 
 // DEVCFG1
-#pragma config FNOSC = PRIPLL // use primary oscillator with pll
-#pragma config FSOSCEN = OFF // turn off secondary oscillator
-#pragma config IESO = OFF // no switching clocks
-#pragma config POSCMOD = HS // high speed crystal mode
-#pragma config OSCIOFNC = OFF  // free up secondary osc pins
-#pragma config FPBDIV = DIV_1 // divide CPU freq by 1 for peripheral bus clock
-#pragma config FCKSM = CSDCMD // do not enable clock switch
-#pragma config WDTPS = PS1048576 // slowest wdt
-#pragma config WINDIS = OFF // no wdt window
-#pragma config FWDTEN = OFF // wdt off by default
+#pragma config FNOSC = PRIPLL       // use primary oscillator with pll
+#pragma config FSOSCEN = OFF        // turn off secondary oscillator
+#pragma config IESO = OFF           // no switching clocks
+#pragma config POSCMOD = HS         // high speed crystal mode
+#pragma config OSCIOFNC = OFF       // free up secondary osc pins
+#pragma config FPBDIV = DIV_1       // divide CPU freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD       // do not enable clock switch
+#pragma config WDTPS = PS1048576    // slowest wdt
+#pragma config WINDIS = OFF         // no wdt window
+#pragma config FWDTEN = OFF         // wdt off by default
 #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the CPU clock to 48MHz
-#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
-#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
-#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
-#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiply by 12 to get 48MHz for USB
-#pragma config UPLLEN = ON // USB clock on
+#pragma config FPLLIDIV = DIV_2      // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24      // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2      // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = DIV_2      // divider for the 8MHz input clock, then multiply by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON           // USB clock on
 
 // DEVCFG3
-#pragma config USERID = 0x1234 // some 16bit userid, doesn't matter what
-#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
-#pragma config IOL1WAY = OFF // allow multiple reconfigurations
-#pragma config FUSBIDIO = ON // USB pins controlled by USB module
-#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
+#pragma config USERID = 0x1234   // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF    // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF     // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON     // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON    // USB BUSON controlled by USB module
 
-#define CS LATBbits.LATB7 // chip select pin
-#define SLAVE_ADDR 0x6B   // slave address 01101011
+#define CS LATBbits.LATB7     // chip select pin
+#define SLAVE_ADDR 0x6B       // slave address 01101011
+#define maxLength 14          // slave address 01101011
 
-unsigned char read  = 0x00;
-unsigned char whoAmI  = 0x00;
-unsigned char checkGP7 = 0x00;
+unsigned char whoAmI  = 0x00;   // check who am I register 
+
+char stuff[maxLength];      // save 14 8 bit data
+char gx = 0x0000;           // gyroscope x
+char gy = 0x0000;           // gyroscope x
+char gz = 0x0000;           // gyroscope x
+char ax = 0x0000;           // gyroscope x
+char ay = 0x0000;           // gyroscope x
+char az = 0x0000;           // gyroscope x
+char temp = 0x0000;         // gyroscope x
 
 //IMU
 void initIMU();
 unsigned char getWhoAmI();
-void I2C_read_multiple(char add, char reg, unsigned char * data, char length);
+void I2C_read_multiple(char add, char reg, char * data, char length);
 
 void __ISR(_TIMER_2_VECTOR, IPL5SOFT) PWMcontroller(void) { // step 1: the ISR
 
@@ -106,17 +114,14 @@ int main() {
     
     RPB15Rbits.RPB15R = 0b0101; // assign OC1 to RB15
     RPA1Rbits.RPA1R = 0b0101; // assign OC2 to RA1 
-    /*
+    
+    
     if(getWhoAmI() == 0x69){
         LATAbits.LATA4 = 1;
     }
-    */
-    unsigned char stuff;
-        I2C_read_multiple(SLAVE_ADDR, 0x0f, &stuff, 1);
     
-    if(stuff == 0x69){
-        LATAbits.LATA4 = 1;
-    }
+    I2C_read_multiple(SLAVE_ADDR, 0x20, &stuff, 14);
+    
     while(1){
         _CP0_SET_COUNT(0);
         
@@ -162,7 +167,7 @@ void initIMU(){  // no need to connect SD0
     i2c_master_stop();
 }
 
-void I2C_read_multiple(char add, char reg, unsigned char * data, char length){
+void I2C_read_multiple(char add, char reg, char * data, char length){
     i2c_master_start();
     i2c_master_send(add << 1);    
     i2c_master_send(reg);
